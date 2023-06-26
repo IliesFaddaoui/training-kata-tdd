03fbcd846c3a35a0fbe06b23742ce1d32f960e68
IliesFaddaoui <faddaoui.i@gmail.com>
2023-06-26 23:18:49 +0200

    Fizzbuzz: all tests

--- src/main/java/com/ilies/trainingkatatdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/ilies/trainingkatatdd/fizzbuzz/FizzBuzz.java
@@ -1,12 +1,21 @@
 package com.ilies.trainingkatatdd.fizzbuzz;
 
 public class FizzBuzz {
-    public String process(int i){
-        if(i == 3){
-            return "Fizz";
+    public String process(int i) {
+        if (i == 0) {
+            return "0";
         }
-        else {
+        var result = "";
+        if (i % 3 == 0) {
+            result += "Fizz";
+        }
+        if (i % 5 == 0) {
+            result += "Buzz";
+        }
+        if (result.isBlank()) {
             return String.valueOf(i);
+        } else {
+            return result;
         }
     }
 }
--- src/test/java/com/ilies/trainingkatatdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/ilies/trainingkatatdd/fizzbuzz/FizzBuzzTest.java
@@ -11,16 +11,39 @@ class FizzBuzzTest {
 
     @Spy
     private FizzBuzz fizzBuzz;
-    // 1st test for fizzBuzz: We take the more general solution which is the default case and the most basic solution as required by TDD methodology, here return the int by default
+
     @Test
-    void shouldReturn0If0(){
+    void shouldReturn0If0() {
         assertEquals("0", fizzBuzz.process(0));
     }
 
-    // 2nd test for fizzbuzz: we should assert for a second use case: should return Fizz if equal 3
     @Test
-    void shouldReturnFizzIf3() {
-        assertEquals("Fizz",fizzBuzz.process(3));
+    void shouldReturn1If1() {
+        assertEquals("1", fizzBuzz.process(1));
     }
 
+    @Test
+    void shouldReturnFizzIf3() {
+        assertEquals("Fizz", fizzBuzz.process(3));
+    }
+
+    @Test
+    void shouldReturnFizzIf6() {
+        assertEquals("Fizz", fizzBuzz.process(6));
+    }
+
+    @Test
+    void shouldReturnBuzzIf5() {
+        assertEquals("Buzz", fizzBuzz.process(5));
+    }
+
+    @Test
+    void shoudlReturnBuzzIf10() {
+        assertEquals("Buzz",fizzBuzz.process(10));
+    }
+
+    @Test
+    void shouldReturnFizzBuzzIf15() {
+        assertEquals("FizzBuzz", fizzBuzz.process(15));
+    }
 }
