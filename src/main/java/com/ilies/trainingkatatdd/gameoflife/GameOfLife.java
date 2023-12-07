2d22635f79ce177d5fce3be3672607af5e6804ec
Ilies Faddaoui <iliesfaddaoui@MacBook-Pro-de-Ilies.local>
2023-12-07 11:49:16 +0100

    implementation of game of life rule 4 + code clean

--- src/main/java/com/ilies/trainingkatatdd/gameoflife/GameOfLife.java
+++ src/main/java/com/ilies/trainingkatatdd/gameoflife/GameOfLife.java
@@ -1,4 +1,61 @@
 package com.ilies.trainingkatatdd.gameoflife;
 
-public class GameOfLife {
+import java.util.Arrays;
+
+public record GameOfLife(Cell[][] grid) {
+
+    public GameOfLife nextGeneration() {
+        int gridSize = grid().length;
+        Cell[][] nextGeneration = new Cell[gridSize][gridSize];
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                int aliveNeighbors = countAliveNeighbors(i, j);
+                if (aliveNeighbors < 2)
+                    nextGeneration[i][j] = Cell.dead();
+                else if (aliveNeighbors > 3)
+                    nextGeneration[i][j] = Cell.dead();
+                else if (aliveNeighbors == 3) {
+                    nextGeneration[i][j] = Cell.alive();
+                } else
+                    nextGeneration[i][j] = grid[i][j];
+            }
+        }
+        return new GameOfLife(nextGeneration);
+    }
+
+    public int countAliveNeighbors(int row, int col) {
+        var size = grid.length - 1;
+        var possibleNeighborsLocations = possibleNeighbors(row, col);
+
+        return Arrays.stream(possibleNeighborsLocations)
+                .reduce(0, (aliveCells, neighbor) -> {
+                    var neighborRow = neighbor[0];
+                    var neighborCol = neighbor[1];
+                    var neighborIsInGrid = 0 <= neighborRow && neighborRow <= size && 0 <= neighborCol && neighborCol <= size;
+                    return neighborIsInGrid && grid[neighborRow][neighborCol].status()
+                            ? Integer.valueOf(aliveCells + 1) : aliveCells;
+                }, (acc, acc2) -> acc);
+    }
+
+    private static int[][] possibleNeighbors(int row, int col) {
+        int previousRow = row - 1;
+        int previousCol = col - 1;
+        int nextCol = col + 1;
+        int nextRow = row + 1;
+        return new int[][]{
+                {previousRow, previousCol},
+                {previousRow, col},
+                {previousRow, nextCol},
+                {row, previousCol},
+                {row, nextCol},
+                {nextRow, previousCol},
+                {nextRow, col},
+                {nextRow, nextCol},
+        };
+    }
+
+    public Cell cellAt(int i, int j) {
+        return grid[i][j];
+    }
+
 }
--- src/test/java/com/ilies/trainingkatatdd/gameoflife/GameOfLifeTest.java
+++ src/test/java/com/ilies/trainingkatatdd/gameoflife/GameOfLifeTest.java
@@ -1,6 +1,9 @@
 package com.ilies.trainingkatatdd.gameoflife;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -9,89 +12,89 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class GameOfLifeTest {
     @Test
-    void grid_composed_by_only_dead_cells_should_not_change_at_next_generation(){
+    void grid_composed_by_only_dead_cells_should_not_change_at_next_generation() {
         Cell[][] cells = {
-                {Cell.dead(),Cell.dead(),Cell.dead()},
-                {Cell.dead(),Cell.dead(),Cell.dead()},
-                {Cell.dead(),Cell.dead(),Cell.dead()},
+                {Cell.dead(), Cell.dead(), Cell.dead()},
+                {Cell.dead(), Cell.dead(), Cell.dead()},
+                {Cell.dead(), Cell.dead(), Cell.dead()},
         };
-        var initGrid = new Grid(cells);
+        var initGrid = new GameOfLife(cells);
 
-        Grid gridAtNextGeneration = initGrid.nextGeneration();
+        GameOfLife gridAtNextGeneration = initGrid.nextGeneration();
 
-        assertThat(gridAtNextGeneration.cellAt(0,0)).isEqualTo(Cell.dead());
+        assertThat(gridAtNextGeneration.cellAt(0, 0)).isEqualTo(Cell.dead());
     }
 
     @Test
-    void cell_with_less_than_two_alive_neighbors_dead(){
+    void rule_1_underpopulation_cell_with_fewer_than_two_alive_neighbors_dies() {
         Cell[][] cells = {
-                {Cell.alive(),Cell.dead(),Cell.dead()},
-                {Cell.dead(),Cell.dead(),Cell.dead()},
-                {Cell.dead(),Cell.dead(),Cell.dead()},
+                {Cell.alive(), Cell.dead(), Cell.dead()},
+                {Cell.dead(), Cell.dead(), Cell.dead()},
+                {Cell.dead(), Cell.dead(), Cell.dead()},
         };
-        var initGrid = new Grid(cells);
+        var initGrid = new GameOfLife(cells);
 
-        Grid gridAtNextGeneration = initGrid.nextGeneration();
+        GameOfLife gridAtNextGeneration = initGrid.nextGeneration();
 
-        assertThat(gridAtNextGeneration.cellAt(0,0)).isEqualTo(Cell.dead());
+        assertThat(gridAtNextGeneration.cellAt(0, 0)).isEqualTo(Cell.dead());
     }
 
     @Test
-    void cell_with_more_than_3_alive_neighbors_dies() {
+    void rule_2_overcrowding_cell_with_more_than_3_alive_neighbors_dies() {
         Cell[][] cells = {
-                {Cell.alive(),Cell.alive(),Cell.dead()},
-                {Cell.alive(),Cell.alive(),Cell.dead()},
-                {Cell.dead(),Cell.dead(),Cell.alive()},
+                {Cell.alive(), Cell.alive(), Cell.dead()},
+                {Cell.alive(), Cell.alive(), Cell.dead()},
+                {Cell.dead(), Cell.dead(), Cell.alive()},
         };
-        var initGrid = new Grid(cells);
+        var initGrid = new GameOfLife(cells);
 
-        Grid gridAtNextGeneration = initGrid.nextGeneration();
+        GameOfLife gridAtNextGeneration = initGrid.nextGeneration();
 
-        assertThat(gridAtNextGeneration.cellAt(1,1)).isEqualTo(Cell.dead());
+        assertThat(gridAtNextGeneration.cellAt(1, 1)).isEqualTo(Cell.dead());
     }
 
     @Test
-    void cell_with_2_or_3_alive_neighbors_lives() {
+    void rule_3_alive_cell_with_2_or_3_alive_neighbors_lives() {
         Cell[][] cells = {
-                {Cell.alive(),Cell.alive(),Cell.dead()},
-                {Cell.alive(),Cell.alive(),Cell.alive()},
-                {Cell.dead(),Cell.dead(),Cell.dead()},
+                {Cell.alive(), Cell.alive(), Cell.dead()},
+                {Cell.alive(), Cell.alive(), Cell.alive()},
+                {Cell.dead(), Cell.dead(), Cell.dead()},
         };
-        var initGrid = new Grid(cells);
+        var initGrid = new GameOfLife(cells);
 
-        Grid gridAtNextGeneration = initGrid.nextGeneration();
+        GameOfLife gridAtNextGeneration = initGrid.nextGeneration();
 
-        assertThat(gridAtNextGeneration.cellAt(0,0)).isEqualTo(Cell.alive());
-        assertThat(gridAtNextGeneration.cellAt(1,2)).isEqualTo(Cell.alive());
+        //alive cell with 3 alive neighbors
+        assertThat(gridAtNextGeneration.cellAt(0, 0)).isEqualTo(Cell.alive());
+        //alive cell with 2 alive neighbors
+        assertThat(gridAtNextGeneration.cellAt(1, 2)).isEqualTo(Cell.alive());
     }
 
     @Test
-    void dead_cell_with__3_alive_neighbors_relives() {
+    void rule_4_dead_cell_with__3_alive_neighbors_relives() {
         Cell[][] cells = {
-                {Cell.dead(),Cell.alive(),Cell.dead()},
-                {Cell.dead(),Cell.alive(),Cell.alive()},
-                {Cell.dead(),Cell.dead(),Cell.dead()},
+                {Cell.dead(), Cell.alive(), Cell.dead()},
+                {Cell.dead(), Cell.alive(), Cell.alive()},
+                {Cell.dead(), Cell.dead(), Cell.dead()},
         };
-        var initGrid = new Grid(cells);
+        var initGrid = new GameOfLife(cells);
 
-        Grid gridAtNextGeneration = initGrid.nextGeneration();
+        GameOfLife gridAtNextGeneration = initGrid.nextGeneration();
 
-        assertThat(gridAtNextGeneration.cellAt(0,2)).isEqualTo(Cell.alive());
+        assertThat(gridAtNextGeneration.cellAt(0, 2)).isEqualTo(Cell.alive());
     }
-
-
 
     @Nested
     @DisplayName("How many alive neighbors")
-    class countAliveNeighbors{
-        private Grid grid;
+    class countAliveNeighbors {
+        private GameOfLife grid;
 
         @BeforeEach
-        void setup(){
-            this.grid = new Grid(new Cell[][]{
-                    {Cell.alive(),Cell.alive(),Cell.alive()},
-                    {Cell.alive(),Cell.alive(),Cell.alive()},
-                    {Cell.alive(),Cell.alive(),Cell.alive()},
+        void setup() {
+            this.grid = new GameOfLife(new Cell[][]{
+                    {Cell.alive(), Cell.alive(), Cell.alive()},
+                    {Cell.alive(), Cell.alive(), Cell.alive()},
+                    {Cell.alive(), Cell.alive(), Cell.alive()},
             });
         }
 
@@ -107,7 +110,7 @@ class GameOfLifeTest {
                 "bottom middle,1,2,5",
                 "bottom right,2,2,3",
         })
-        void forCellAtTopRight(String displayName, int row, int col, int expectedAliveNeighbors){
+        void forCellAtTopRight(String displayName, int row, int col, int expectedAliveNeighbors) {
 
             var aliveNeighbors = grid.countAliveNeighbors(row, col);
 
